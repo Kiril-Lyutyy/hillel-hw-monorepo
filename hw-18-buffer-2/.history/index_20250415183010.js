09d5324044cef93bbf3c4596b5f4f049c968bcfb
Kirill Lyutyy <kirill.lyutyy@dataart.com>
2025-04-15 18:32:24 +0100

    added HW-18

---
+++ hw-18-buffer-2/.history/index_20250415183010.js
@@ -0,0 +1,8 @@
+const message = 'Hello again, binary world!';
+const buffer = Buffer.from(message, 'utf8');
+const helloBuffer = Buffer.alloc(5);
+
+buffer.copy(helloBuffer, 0, 0, 5);
+
+console.log('Original message:', message);
+console.log('Modified with buffer:', helloBuffer.toString());
---
+++ hw-18-buffer-2/.history/index_20250415183129.js
@@ -0,0 +1,10 @@
+const message = 'Hello again, binary world!';
+const buffer = Buffer.from(message, 'utf8');
+const helloBuffer = Buffer.alloc(5);
+
+buffer.copy(helloBuffer, 0, 0, 5);
+
+const conver
+
+console.log('Original message:', message);
+console.log('Modified with buffer:', helloBuffer.toString());
